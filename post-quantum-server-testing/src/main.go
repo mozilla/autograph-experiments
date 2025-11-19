17102ad54fa936c3eae5ee489c18f2bd45f1ed92
Haseeb Afzal <hafzal@mozilla.com>
2025-11-19 11:38:51 -0500

    added medium payload signature generation tests for all algorithms

---
+++ post-quantum-server-testing/src/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"server-testing/ecdsa"
+	"server-testing/mldsa"
+	"server-testing/rsa"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	smallInput = "aGkK" // "hi" (<1MB)
+)
+
+var (
+	mediumInput = strings.Repeat("aGkK", 1024*512) // "hi" repeatedly (1-2MB)
+)
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+	// Load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func testMldsaSmallPayload(iterations int, location string, keyRing string, projectID string) {
+
+	fmt.Printf("Testing Small Payload ML-DSA-65:\n")
+	keyName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/mldsa/cryptoKeyVersions/1", projectID, location, keyRing)
+	// Get the time pre-signing
+	start := time.Now()
+	for range iterations {
+		_ = mldsa.SignData(smallInput, "mldsa", "null", keyName)
+	}
+	// Get the time post-signing
+	elapsed := time.Since(start)
+	fmt.Printf("Signing time is: %.3f ms\n", elapsed.Seconds()*1000)
+
+	//fmt.Printf("Completed")
+}
+
+func testMldsaMediumPayload(iterations int, location string, keyRing string, projectID string) {
+
+	fmt.Printf("Testing Medium Payload ML-DSA-65:\n")
+	keyName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/mldsa/cryptoKeyVersions/1", projectID, location, keyRing)
+	// Get the time pre-signing
+	start := time.Now()
+	for range iterations {
+		_ = mldsa.SignData(mediumInput, "mldsa", "null", keyName)
+	}
+	// Get the time post-signing
+	elapsed := time.Since(start)
+	fmt.Printf("Signing time is: %.3f ms\n", elapsed.Seconds()*1000)
+
+	//fmt.Printf("Completed")
+}
+
+func testEcdsaSmallPayload(iterations int, location string, keyRing string, projectID string) {
+
+	fmt.Printf("Testing Small Payload ECDSA-384:\n")
+	keyName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/ecdsa/cryptoKeyVersions/1", projectID, location, keyRing)
+	// Get the time pre-signing
+	start := time.Now()
+	for range iterations {
+		_ = ecdsa.SignData(smallInput, "ecdsa", "null", keyName)
+	}
+	// Get the time post-signing
+	elapsed := time.Since(start)
+	fmt.Printf("Signing time is: %.3f ms\n", elapsed.Seconds()*1000)
+
+	//fmt.Printf("Completed")
+}
+
+func testEcdsaMediumPayload(iterations int, location string, keyRing string, projectID string) {
+
+	fmt.Printf("Testing Medium Payload ECDSA-384:\n")
+	keyName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/ecdsa/cryptoKeyVersions/1", projectID, location, keyRing)
+	// Get the time pre-signing
+	start := time.Now()
+	for range iterations {
+		_ = ecdsa.SignData(mediumInput, "ecdsa", "null", keyName)
+	}
+	// Get the time post-signing
+	elapsed := time.Since(start)
+	fmt.Printf("Signing time is: %.3f ms\n", elapsed.Seconds()*1000)
+
+	//fmt.Printf("Completed")
+}
+
+func testRsaSmallPayload(iterations int, location string, keyRing string, projectID string) {
+
+	fmt.Printf("Testing Small Payload RSA-4096:\n")
+	keyName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/rsa/cryptoKeyVersions/1", projectID, location, keyRing)
+	// Get the time pre-signing
+	start := time.Now()
+	for range iterations {
+		_ = rsa.SignData(smallInput, "rsa", "null", keyName)
+	}
+	// Get the time post-signing
+	elapsed := time.Since(start)
+	fmt.Printf("Signing time is: %.3f ms\n", elapsed.Seconds()*1000)
+
+	//fmt.Printf("Completed")
+}
+
+func main() {
+	iterations := 10
+	location := goDotEnvVariable("LOCATION")
+	keyRing := goDotEnvVariable("KEYRING")
+	projectID := goDotEnvVariable("PROJECT_ID")
+
+	testRsaSmallPayload(iterations, location, keyRing, projectID)
+}
